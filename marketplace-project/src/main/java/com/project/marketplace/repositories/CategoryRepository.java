d8de25b4179d463901fb41a5a932b2231b0b4205
Gabriel Krauss Costa <43766682+GabrielKrauss@users.noreply.github.com>
2026-04-08 21:48:46 -0300

    Add files via upload

---
+++ marketplace-project/src/main/java/com/project/marketplace/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.project.marketplace.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.marketplace.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	boolean existsByName(String name);
+}
